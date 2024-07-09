cc37bda2fb4fb84479711c6020bfb0f4c5e57cc5
Chris <ccolon29@mail.valenciacollege.edu>
2024-07-08 20:01:21 -0400

    created prompts when the add employee btn is clicked

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,9 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  const firstName = window.prompt("Enter first name:");
+  const lastName = window.prompt("Enter last name:");
+  const salary = window.prompt("Enter salary:");
 }
 
 // Display the average salary
